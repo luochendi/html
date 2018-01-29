694c12121d7df3118b981a56f8f6ffed6c459dbc
chendiLuo <30401824+luochendi@users.noreply.github.com>
2018-01-29 16:26:31 +0800

    登录验证

--- luo/js/login.js
+++ luo/js/login.js
@@ -1,17 +1,79 @@
 $(function() {
-    $(".error").hide();
+    // $(".error").hide();
+
     $(".btn").click(function () {
-        var accNode = $("#account");
-        var pswNode = $("#psw");
-        var acc = accNode.val();
-        var psw = pswNode.val();
+        // else{
+        //     $(".error").show();
+        // }
+        // $.ajax({
+        //     url:"login.jsp",
+        //     data:{
+        //         acc:acc,
+        //         psw:psw
+        //     },
+        //     type:"POST",
+        //     success:function(data){
+        //
+        //     },
+        //     error:function(){
+        //
+        //     }
+        // })
+        var acc = $("#account").val();
+        var psw = $("#psw").val();
+        if(!acc){
+            $("#v-acc").html("用户名不能为空！");
+            return;
+        }
+        else{
+            $("#v-acc").html("");
+        }
+
+        if(psw==""){
+            $("#v-psw").html("密码不能为空！");
+            return;
+        }
+        else{
+            $("#v-psw").html("");
+        }
+
         if(acc == "admin"&&psw == "admin"){
             window.location.href = "frame.html";
         }
         else{
-            $(".error").show();
+            alert("用户名或密码不正确！");
         }
-    });
-
+        })
 });
+
+
+// $.fn.GetSelect = function()
+// {
+//     var ui = $(this).find("ul");
+//     return ul.find("li.active").attr("data-id");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
