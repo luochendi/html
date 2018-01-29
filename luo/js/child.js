647cbc64733fef23048b13b455cdfde8b92d7a64
chendiLuo <30401824+luochendi@users.noreply.github.com>
2018-01-29 11:59:45 +0800

    合并.js

--- luo/js/child.js
+++ luo/js/child.js
@@ -12,6 +12,14 @@ $(function() {
         }
     });
 
+    loadTr();
+
+    function loadTr() {
+        for (var i = 0; i < 10; i++) {
+            $("#tbody").append("<tr > <td ><a  class=\"checkbox\"></a>江苏微睿特信息科技有限公司</td> <td>微睿微睿特微睿信息巡检管理系统</td><td>巡检、签到</td><td>张某某、李某某、徐某某、成某某、周某某、张某某</td><td>未激活</td></tr>");
+        }
+    }
+
     $("#new").hide();
     $("#mid-bg").hide();
 
@@ -37,9 +45,12 @@ $(function() {
         window.top.setBg(0);
     });
 
-    $(".exit,.pic-exit").click(function(){
-        window.location.href = "login.html";
+    $(".user").click(function(){
+        window.location.href = "child2.html";
     });
 
+    $(".manage").click(function(){
+        window.location.href = "child1.html";
+    });
 
 });
--- luo/js/frame.js
+++ luo/js/frame.js
@@ -2,19 +2,6 @@ $(document).ready(function() {
     $(".child2").hide();
     $("#mid-pbg").hide();
 
-    // $(".user").click(function () {
-    //     $(".child1").hide();
-    //     $(".child2").show();
-    //     $(".ma  img").attr("src", "imgs/manage/manage1.png");
-    //     $(".us  img").attr("src", "imgs/manage/user2.png");
-    // });
-    // $(".manage").click(function () {
-    //     $(".child2").hide();
-    //     $(".child1").show();
-    //     $(".ma  img").attr("src", "imgs/manage/manage2.png");
-    //     $(".us  img").attr("src", "imgs/manage/user1.png");
-    // });
-
     $(".mid-center a").click(function(){
         var _src = $(this).attr("frame-src");
         $("#frame").attr("src",_src);
@@ -24,5 +11,7 @@ $(document).ready(function() {
     $(".exit,.pic-exit").click(function(){
         window.location.href = "login.html";
     });
+
+
 });
 
